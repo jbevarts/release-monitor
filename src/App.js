f17895ce643e39b444f46b92d8ae71f500d595e6
Jerry Evarts <jerryevarts@gmail.com>
2020-12-04 18:25:26 -0800

    added sorting and update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+// import router from 'react-router';
 import { Octokit } from "@octokit/core";
 import ButtonForm from "./Components/ButtonForm";
 import { FaSearchPlus } from 'react-icons/fa';
@@ -14,6 +15,7 @@ class App extends Component {
     var releaseMonitorData = JSON.parse(localStorage.getItem('release-monitor-data'));
     this.state = {data: releaseMonitorData};
   }
+
 
   handleSeen = (id) => {
     let seen = this.state.data.find((elem) => elem.id === id);
@@ -40,14 +42,16 @@ class App extends Component {
         let releaseData = this.state.data === null ? [] : this.state.data;
         let exists = releaseData.find((elem) => elem.id === response.data[0].id );
         if (!exists) {
-          releaseData = releaseData.concat(
+          let newData = [
             {
               owner: owner,
               repository: repo,
               id: response.data[0].id,
               seen: false,
               releaseNotes: response.data[0].body
-            })
+            }];
+          releaseData = newData.concat(releaseData);
+        
         } 
         this.updateState(releaseData);
       }
@@ -59,6 +63,79 @@ class App extends Component {
     this.setState({data: releaseData});
     localStorage.setItem('release-monitor-data', JSON.stringify(releaseData));
   }
+  componentDidUpdate(prevState) {
+    console.log("component udating");
+    console.log(prevState);
+    console.log(this.state.data[0]);
+  }
+
+  handleReload(event) {
+    // event.preventDefault();
+    //this.updateState(null);
+    this.handleReloadHelper(event);
+    console.log("outer wrapper");
+  }
+
+  handleReloadHelper(event) {
+    let currentState = this.state.data;
+    var promises = [];
+
+    currentState.forEach((elem, index) => {
+      promises.push(
+        octokit.request('GET /repos/{owner}/{repo}/releases', {
+          owner: elem.owner,
+          repo: elem.repository
+        })
+        .then((response) => {
+          if (response.data[0].id !== elem.id) {
+            let newElem = elem;
+            newElem.releaseNotes = response.data[0].body;
+            newElem.id = response.data[0].id;
+            newElem.seen = false;
+            //tempState.data.push(elem);
+            console.log(elem.seen);
+            return newElem;
+          } else {
+            let newElem = elem;
+            return newElem;
+            //tempState.data.push(elem);
+          }
+        })
+      )
+    });
+    Promise.all(promises).then((finishedPromises) => {
+      let stateArray = [];
+      // promises.forEach((elem,index) => {
+      //   elem.then((response) => {
+      //     console.log(" inside here" + response);
+      //     stateArray.push(response);
+      //   }).then(() => {
+      //     if (index === promises.length - 1) {
+      //       this.updateState(stateArray);
+      //       console.log('update baby');
+      //     }
+      //   })
+      // });
+        console.log(finishedPromises);
+        this.handleShuffle(finishedPromises);
+        this.updateState(finishedPromises);
+        window.location.reload();
+        console.log("end of update function");
+    });
+    console.log("exiting");
+}
+
+handleShuffle(array) {
+  array.sort((a, b) => {
+    if (a.seen && b.seen) {
+      return 0;
+    } else if (a.seen) {
+      return 1;
+    } else if (b.seen) {
+      return -1;
+    }
+  })
+}
 
   render() {
     return (
@@ -79,6 +156,7 @@ class App extends Component {
             })}
           </div>
         </div>
+        <button onClick={this.handleReload.bind(this)}>some text int he div</button>
         <div className='right_panel'></div>
       </div>
     );
